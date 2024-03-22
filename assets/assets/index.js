0230349242be141358fe36213d2f44a640e1cdfa
kelvin banda <120105858+kevtheprogrammer@users.noreply.github.com>
2024-03-22 16:40:21 +0200

    adding assets

---
+++ assets/assets/index.js
@@ -0,0 +1,68 @@
+import airbnb from "./airbnb.png";
+import bill from "./bill.png";
+import binance from "./binance.png";
+import card from "./card.png";
+import card1 from "./card1.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+// import logo from "./logo.svg";
+import logo from "./logo.png"
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import robot1 from "./robot1.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+import sureLink from "./sureLink.png";
+import loanLink from "./loanLink.png";
+import healtLink from "./healtLink.png";
+import clientLink from "./clientLink.png";
+import webDesign from "./webDesign.png";
+
+export {
+  clientLink,
+  webDesign,
+  sureLink,
+  loanLink,
+  healtLink,
+  airbnb,
+  bill,
+  binance,
+  card,
+  card1,
+  coinbase,
+  dropbox,
+  logo,
+  quotes,
+  robot,
+  robot1,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+};
